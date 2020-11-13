c5cb5586bc92c5c2c297bd5d0ce0d4c360df8d74
jhuynh24 <jhuynh1735@gmail.com>
2020-11-12 23:07:33 -0600

    adding questions

--- Unsolved/index.js
+++ Unsolved/index.js
@@ -3,7 +3,52 @@ const generateMarkdown = require('./utils/generateMarkdown')
 const fs = require('fs');
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is the title to your project?',
+        name: 'title',
+      },
+      {
+          type: 'input',
+          message: 'Describe your project.',
+          name: 'description'
+      },
+      {
+        type: 'input',
+        message: 'What would you like your install instructions to be?',
+        name: 'installInstructions'
+    },
+    {
+        type: 'input',
+        message: 'Enter your usage information.',
+        name: 'usageInfo'
+    },
+    {
+        type: 'input',
+        message: 'What are your contribution guidelines?',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'What are your testing instructions?',
+        name: 'testInstructions'
+    },
+    {
+        type: 'list',
+        message: 'What license would you like?',
+        name: 'license',
+        choices: ['a','b','c']
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'userName'
+    },
+    {
+        type: 'input',
+        message: 'What is your e-mail?',
+        name: 'email'
+    }
 ];
 
 // function to write README file
