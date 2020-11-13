3303dd60ff7cf153b0a0140f57badb05af234c90
jhuynh24 <jhuynh1735@gmail.com>
2020-11-12 23:08:37 -0600

    adding function for generating markdown

--- Unsolved/utils/generateMarkdown.js
+++ Unsolved/utils/generateMarkdown.js
@@ -2,6 +2,35 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+### Description
+${data.description}
+
+### Table of Contents
+[Description](#Description)
+[Installation](#Installation)
+[Usage](#Usage)
+[License](#License)
+[Contributing](#Contributing)
+[Tests](#Tests)
+[Questions](#Questions)
+
+
+### Installation
+${data.installInstructions}
+
+
+### Usage
+${data.usageInfo}
+
+### License
+${data.license}
+### Contributing
+${data.contribution}
+### Tests
+${data.testInstructions}
+### Questions
+[Github page](https://github.com/${data.userName})
+With further questions, please e-mail <${data.email}>
 `;
 }
 
